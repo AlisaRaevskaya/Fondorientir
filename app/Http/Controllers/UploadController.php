bbb99ec959606e2071d13eb5e9e1ca2580c0644f
Alisa Raevskaya <vindicta90@mail.ru>
2020-11-18 23:07:10 +0300

    new

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\News;
 use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 
